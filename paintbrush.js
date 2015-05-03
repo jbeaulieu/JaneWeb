f2253456edcb21016ea98824f2910c1a283c2ba7
Emily Giurleo <egiurleo@mit.edu>
2015-05-03 14:04:56 -0400

    Added borders to paintbrush and garbagecan

--- paintbrush.js
+++ paintbrush.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   var colorshowing = false;
 
   $("#paintbrush").mouseover(function(){
+
     if(!colorshowing){
-      $(this).css("opacity", "1");
+
     } 
   });
 
   $("#paintbrush").mouseleave(function(){
     if(!colorshowing){
-      $(this).css("opacity", ".5");
+
     }
   });
 
@@ -23,10 +24,8 @@ $(document).ready(function(){
 
     if(colorshowing === true){
       $(".color").css("visibility", "visible");
-      $(this).css("opacity", "1");
     }else{
       $(".color").css("visibility", "hidden");
-      $(this).css("opacity", ".5");
     }
     
     

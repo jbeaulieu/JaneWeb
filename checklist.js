471e6703ebfff571799c5ac9078c4f031564ce59
Alexis Dien <adien@mit.edu>
2015-05-04 07:01:34 -0400

    Added Checklist abilities
    Made the font into a monospace in order to be able to limit the length
    of the notes/checklist items properly.
    Added checklist implementation up to 12 items (otherwise would overflow
    the list area).
    Dynamical check the length of the note to make sure it doesn’t get
    longer than 6 lines (would otherwise overflow).
    Need to: disallow the child to edit the notes and add items to list.

--- checklist.js
+++ checklist.js
@@ -7,16 +7,22 @@ $(document).ready(function(){
 // code for checklist inspired by Ronalds Vilcins. Found at
 // http://codepen.io/RonaldsVilcins/pen/iJxGB
 
+//list title has been clicked to edit
 var listTitleClicked = function(list_id){
     var top = $("#"+list_id+"Title").css("top");
     var left = $("#"+list_id+"Title").css("left");
     var titleText = $("#"+list_id+"Title").html();
 
-    var editableTitleText = $("<textarea id='"+ list_id + "Title'></textarea>");
+    var editableTitleText = $("<textarea id='"+ list_id + "Title' class='list_Title mono_font' maxlength='17'></textarea>");
 
     $("#"+list_id+"Title").replaceWith(editableTitleText);
     $("#"+list_id+"Title").css({"top":top, "left":left});
     $("#"+list_id+"Title").html(titleText);
+    $("#"+list_id+"Title").keypress(function (event){
+      if (event.which == 13){
+        editableListTitleBlurred(list_id);
+      }
+    });
     editableTitleText.focus();
     // setup the blur event for this new textarea
     $("#"+list_id+"Title").on("blur",function (event, ui) {
@@ -24,28 +30,15 @@ var listTitleClicked = function(list_id){
       });
 }
 
-var listItemsClicked = function(list_id){
-	$('#'+list_id+'input').css("visibility", "visible");
-	$('#'+list_id+'add-checklist-item').css("visibility", "visible");
-	var top = $("#"+list_id+"").css("top");
-    var left = $("#"+list_id+"").css("left");
-	var itemText = $("#"+item_id+"Item").html();
-    
-    $(list_id+"Items").each(function (index) {
-
-    });
-}
-
-var listCheckboxClicked = function(list_id, item_id){
-
-}
-
+//list title is finished being edited
 var editableListTitleBlurred = function(list_id){
+	$('#'+list_id+'input').css("visibility", "hidden");
+	$('#'+list_id+'add-checklist-item').css("visibility", "hidden");
     var top = $("#"+list_id+"Title").css("top");
     var left = $("#"+list_id+"Title").css("left");
     var text = $("#"+list_id+"Title").val();
 
-    var viewableText = $("<h4 id ='"+list_id+"Title'> </h4>");
+    var viewableText = $("<h4 id ='"+list_id+"Title' class='mono_font'> </h4>");
     $("#"+list_id+"Title").replaceWith(viewableText);
     $("#"+list_id+"Title").css({"top":top, "left":left});
     $("#"+list_id+"Title").html(text);
@@ -55,44 +48,38 @@ var editableListTitleBlurred = function(list_id){
     });
 }
 
-$("body").on('click', '#list a', function () {
-	$(this).closest("li").remove();
-});
-
-var addChecklistItem = function (list_id){
-	console.log($("#"+list_id+"Input").val());
-	$("<li>" + $("#"+list_id+"Input").val() + " <a id='deleter' href='#' class='close' aria-hidden='true'>&times;</a></li>").appendTo("#"+list_id+"Items")
-	$("#"+list_id+"Input").val("");
-	$("#"+list_id+"Input").focus();
+//The area right below the list has been clicked to open the edit of the list 
+var listItemsClicked = function(list_id){
+	if ($("#"+list_id+"Items li").length <12){
+		$('#'+list_id+'input').focus();
+		$('#'+list_id+'input').css("visibility", "visible");
+		$('#'+list_id+'add-checklist-item').css("visibility", "visible");
+	}
+	$("#"+list_id+"input").on("blur",function (event, ui) {
+		editableListItemsBlurred(list_id);
+    });
 }
 
-  // <!-- modal for add-checklist -->
-  //   <div class="modal fade" id="add-checklist-modal" tabindex="-1" role="dialog" aria-labelledby="add-checklist-modal" aria-hidden="true">
-  //     <div class="modal-dialog">
-  //       <div class="modal-content">
-  //         <div class="modal-header">
-  //           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  //           <h4 class="modal-title" id="add-checklist-modal-label">Add Checklist</h4>
-  //         </div>
-  //         <div class="modal-body">
-  //           <div class="input-group">
-  //             <span class="input-group-addon">Checklist Title:</span>
-  //             <input type="text" id="checklistTitle" class="form-control" aria-describedby="checklistTitle">
-  //           </div>
-  //           <!-- <div class="form-group"> -->
-  //           <div>
-  //             <!-- <form role="form"> -->
-  //             <!-- <form> -->
-  //               <input id = "item" type="text" class="form-control" placeholder="New item" name="item">
-  //               <button type="submit" id="add-checklist-item" class="btn btn btn-primary">Add</button>
-  //             <!-- </form> -->
-  //           <ul class="list-unstyled" id="list"></ul>
-  //         </div>
-  //       </div>
-  //       <div class="modal-footer">
-  //         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-  //         <button type="button" id="add-checklist-btn" class="btn btn-primary" data-dismiss="modal">Finish</button>
-  //     </div>
-  //     </div>
-  //   </div>
-  // </div>
+//The input field of the list has been blurred so the user is no longer adding items to the list
+var editableListItemsBlurred = function(list_id){
+	$('#'+list_id+'input').css("visibility", "hidden");
+	$('#'+list_id+'add-checklist-item').css("visibility", "hidden");
+	$("#"+list_id+"editableItems").on("click",function (event, ui) {
+		listItemsClicked(list_id);
+    });
+}
+
+//the add button is clicked
+var addChecklistItem = function (list_id){
+	//check that the item to add is not an empty string and that the list is shorter than 12 items
+	if ($("#"+list_id+"input").val() != "" && $("#"+list_id+"Items li").length <12){
+		$("<li>" + $("#"+list_id+"input").val() + " <a id='deleter' href='#' class='close' aria-hidden='true'>&times;</a></li>").appendTo("#"+list_id+"Items")
+		$("#"+list_id+"input").val("");
+		$("#"+list_id+"input").focus();
+	}
+	//If the list is too long, force a blur of the input field (causing the add-ability to go away)
+	if ($("#"+list_id+"Items li").length >=12){
+		$("#"+list_id+"input").blur();
+		editableListItemsBlurred(list_id);
+	}
+}
